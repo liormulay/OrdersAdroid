8bc38b0c9107263058419a839ccb75f6a8651e2b
liorm <liormualy6@gmail.com>
2020-07-18 22:52:55 +0300

    finish ApprovalOrderActivity

--- app/src/main/java/com/example/orders/views/activities/ApprovalOrderActivity.java
+++ app/src/main/java/com/example/orders/views/activities/ApprovalOrderActivity.java
@@ -1,16 +1,40 @@
 package com.example.orders.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.orders.R;
+import com.example.orders.model.Order;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import static com.example.orders.views.activities.OrdersActivity.ORDER;
 
 public class ApprovalOrderActivity extends AppCompatActivity {
+
+    private AppCompatTextView messageTextView;
+
+    private AppCompatButton homePageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_approval_order);
+        messageTextView = findViewById(R.id.message_textView);
+        homePageButton = findViewById(R.id.homepage_button);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Order order = (Order) extras.getSerializable(ORDER);
+            if (order != null) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", new Locale("he"));
+                messageTextView.setText(String.format("Your order will arrive at %s", dateFormat.format(order.getShipDate())));
+            }
+        }
+        homePageButton.setOnClickListener(v -> startActivity(new Intent(ApprovalOrderActivity.this, HomePageActivity.class)));
     }
 }
