5e8dfd9844ef8792e6ecc76ce40430d19dafc4b7
liorm <liormualy6@gmail.com>
2020-07-30 19:52:51 +0300

    sort by date

--- app/src/main/java/com/example/orders/viewmodels/OrdersViewModel.java
+++ app/src/main/java/com/example/orders/viewmodels/OrdersViewModel.java
@@ -10,14 +10,24 @@ import com.example.orders.utils.SharedPreferencesUtils;
 
 import java.util.List;
 
+import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
 
+
 public class OrdersViewModel extends ViewModel {
 
+    /**
+     *
+     * @param context
+     * @return orders sorted descendant by their date
+     */
     public Single<List<Order>> getUserOrders(Context context) {
         return NetworkClient.getNetworkInterface()
                 .getOrders(SharedPreferencesUtils.retrieveToken(context))
+                .flatMapObservable(Observable::fromIterable)
+                .sorted((order1, order2) -> order2.getOrderDate().compareTo(order1.getOrderDate()))
+                .toList()
                 .subscribeOn(Schedulers.io());
     }
 }
